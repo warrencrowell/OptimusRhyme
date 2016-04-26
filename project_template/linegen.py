736f045cc765c51d0adbf2394400624960f6ff1e
Ryan Vogan <voganrc@gmail.com>
2016-04-25 22:38:56 -0400

    made lines roughly same length

--- project_template/linegen.py
+++ project_template/linegen.py
@@ -6,7 +6,7 @@ def get_random_line(corpus, last_line = None):
 		line = random.choice(song)
 		if not True in['nigg' in low_word for low_word in [word.lower() for word in line]]:
 			if last_line:
-				if abs(len(last_line) - len(line)) <= 5:
+				if abs(len(last_line) - len(line)) <= 3:
 					break
 			elif len(line) >= 5 and len(line) <= 15:
 				break
