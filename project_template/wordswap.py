14196025428e52b6111a0fa6860a7f92b4658ec1
Ryan Vogan <voganrc@gmail.com>
2016-04-26 01:31:16 -0400

    stupid indents

--- project_template/wordswap.py
+++ project_template/wordswap.py
@@ -5,21 +5,21 @@ REMOVE_SYMS = ['[', ']', '(', ')']
 MERGE_SYMS = [',', '\'', '!', '.', '?']
 
 def replace_random_word(line, candidate_words):
-	new_line = list(line)
-	pos = nltk.pos_tag(line)
-	noun_idxs = []
-	for i in range(len(line)):
-		if 'NN' in pos[i][1]:
-			noun_idxs.append(i)
-	if len(noun_idxs) == 0:
-		return line
-	else:
-		rand_noun_idx = random.choice(noun_idxs)
+    new_line = list(line)
+    pos = nltk.pos_tag(line)
+    noun_idxs = []
+    for i in range(len(line)):
+        if 'NN' in pos[i][1]:
+            noun_idxs.append(i)
+    if len(noun_idxs) == 0:
+        return line
+    else:
+        rand_noun_idx = random.choice(noun_idxs)
         old_noun = new_line[rand_noun_idx]
-		new_line[rand_noun_idx] = '<font color=\"green\">' + random.choice(candidate_words) + '</font>'
+        new_line[rand_noun_idx] = '<font color=\"green\">' + random.choice(candidate_words) + '</font>'
         new_line.append("|")
         new_line.append('<font color=\"red\">' + old_noun + '</font>')
-		return new_line
+        return new_line
 
 def remove_punctuation(line):
     for i in range(len(line)):
@@ -42,7 +42,7 @@ def merge_punctuation(line, start_idx):
 
 
 def format_lines(lines):
-    new_lines = list(lines)	
+    new_lines = list(lines) 
     for i in range(len(new_lines)):
         curr_line = new_lines[i]
         curr_line = remove_punctuation(curr_line)
