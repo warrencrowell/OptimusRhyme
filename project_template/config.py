296cc910b9cc50c5c36e56ba1b55402890e749d6
Elly Nakahara <ellynakahara@gmail.com>
2016-04-27 15:15:30 -0400

    Removing config and other tweaks

--- project_template/config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "86umyiHNBH5KbCiu3VFxHWWnC"
-consumer_secret = "c34yIvlMCSQ1tWSL8BegEcggv9xsS7VmiBIZ2EwIZNpnwht7Kp"
-access_key = "721850611768107008-WjUI9LkoYx79FqmTHpb2cp41yRYfvg4"
-access_secret = "XwXye9Qiu4ASULK8WSmQqzDZESKhRLZQ7HJUimwM3gT6X"
