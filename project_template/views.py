e10ce84e2488a2352a34d5262b75ce821da6ee9d
Elly Nakahara <ellynakahara@gmail.com>
2016-04-29 17:59:11 -0400

    Final version now uses updated tfidf method

--- project_template/views.py
+++ project_template/views.py
@@ -47,7 +47,27 @@ def index(request):
                 output_list = format_lines(output_list)
 
         elif algorithm == 'final':
-            output_list = ['Final system is currently in construction. Please use the prototype version for now.']
+            TM = TweetMining(method = 'tf_idf_new')
+            tweetwords = TM.get_topical_words(hashtags)
+
+            if len(tweetwords) == 0:
+                output_list = ['Not enough tweets are associated with the input hashtag(s). Please try again.']
+            else:
+                ### Load corpus ###
+                if os.path.isfile('dataset.json'):
+                    json_data = open('dataset.json').read()
+                else:
+                    json_data = open('project_template/dataset.json').read()
+                lyrics = json.loads(json_data)
+
+                ### Generate lyrics
+                output_list = [replace_random_word(get_random_line(lyrics),tweetwords)]
+                for i in range(7):
+                    line = get_random_line(lyrics, output_list[-1])
+                    altered_line = replace_random_word(line, tweetwords)
+                    output_list.append(altered_line)
+                
+                output_list = format_lines(output_list)
 
         ### End of our code ###
         paginator = Paginator(output_list, 10)
