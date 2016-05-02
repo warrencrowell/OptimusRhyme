61d50a1dfdc8bac6f10ce2d962a242900f9480ef
Warren Crowell <wmc62@cornell.edu>
2016-05-02 18:38:18 -0400

    favicon

--- project_template/urls.py
+++ project_template/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
+from django.views.generic.base import RedirectView
 from . import views
 
 app_name = 'pt'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico', permanent=True))
 ]
