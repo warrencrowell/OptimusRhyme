9096a0258a6a34438b4c6ac0c3857f964984e710
Ryan Vogan <voganrc@gmail.com>
2016-05-02 21:24:16 -0400

    testing with added dataset

--- project_template/linegen.py
+++ project_template/linegen.py
@@ -30,3 +30,22 @@ def get_random_line(corpus, last_line = None):
             elif len(line) >= 5 and len(line) <= 15:
                 break
     return line
+
+def new_random_line(corpus, tf_idf, last_line = None):
+    while True:
+        song_idx = random.choice(range(len(corpus)))
+        song = corpus[song_idx]
+        line_idx = random.choice(range(len(song)))
+        line = song[line_idx]
+        pos = nltk.pos_tag(line)
+        noun_found = False
+        for i in range(len(line)):
+            if 'NN' in pos[i][1]:
+                noun_found = True
+        if noun_found and not is_explicit(line): 
+            if last_line:
+                if abs(len(last_line) - len(line)) <= 3:
+                    break
+            elif len(line) >= 5 and len(line) <= 15:
+                break
+    return (line, tf_idf[song_idx][line_idx])
--- project_template/test.py
+++ project_template/test.py
@@ -3,33 +3,6 @@ from tweetmining import *
 from linegen import *
 from wordswap import *
 
-
-# nltk.data.path.append('nltk_data/')
-# search = "ham sandwich"
-
-# ### Get tweet words ###
-# hashtags = nltk.word_tokenize(search)
-# TM = TweetMining()
-# tweetwords = TM.get_topical_words(hashtags)
-
-# ### Load corpus ###
-# if os.path.isfile('dataset.json'):
-#     json_data = open('dataset.json').read()
-# else:
-#     json_data = open('project_template/dataset.json').read()
-# lyrics = json.loads(json_data)
-
-# ### Generate lyrics
-# output_list = [replace_random_word(get_random_line(lyrics),tweetwords)]
-# for i in range(7):
-# 	line = get_random_line(lyrics, output_list[-1])
-# 	altered_line = replace_random_word(line, tweetwords)
-# 	output_list.append(altered_line)
-# output_list = format_lines(output_list)
-
-# for output in output_list:
-# 	print output
-
 nltk.data.path.append('nltk_data/')
 search = "trump"
 
@@ -39,16 +12,22 @@ TM = TweetMining(method="tf_idf_new")
 tweetwords = TM.get_topical_words(hashtags)[0]
 
 ### Load corpus ###
-if os.path.isfile('dataset.json'):
-    json_data = open('dataset.json').read()
+if os.path.isfile('lyrics_dataset.json'):
+    json_data = open('lyrics_dataset.json').read()
 else:
-    json_data = open('project_template/dataset.json').read()
+    json_data = open('project_template/lyrics_dataset.json').read()
 lyrics = json.loads(json_data)
 
+if os.path.isfile('scores_dataset.json'):
+    json_data = open('scores_dataset.json').read()
+else:
+    json_data = open('project_template/scores_dataset.json').read()
+song_tfidf = json.loads(json_data)
+
 ### Generate lyrics
-output_list = [wordswap(get_random_line(lyrics),tweetwords)]
+output_list = [wordswap(new_random_line(lyrics,song_tfidf),tweetwords)]
 for i in range(7):
-	line = get_random_line(lyrics, output_list[-1])
+	line = new_random_line(lyrics,song_tfidf,output_list[-1])
 	altered_line = wordswap(line, tweetwords)
 	output_list.append(altered_line)
 output_list = format_lines(output_list)
